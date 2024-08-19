ff53b6d30ce2f23df87f38cd8130f6e295ba084a
Mai-Nhien Dang <mainhied@usc.edu>
2024-08-18 22:40:10 -0700

    Updates

---
+++ precache-manifest.a5c5e4c6e139d7c9079677a5a3ed6f5e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f12881d61ef9efb86ad7b2a020123a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f7611edb3896da300c",
+    "url": "/static/css/main.8884327c.chunk.css"
+  },
+  {
+    "revision": "224678e30f0cfe835f19",
+    "url": "/static/js/2.e1a9645e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e1a9645e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f7611edb3896da300c",
+    "url": "/static/js/main.e7fcc2ee.chunk.js"
+  },
+  {
+    "revision": "5acdee59b20f1a1d8163",
+    "url": "/static/js/runtime-main.fc36865a.js"
+  },
+  {
+    "revision": "7650d1a917af33df6bda5ee7b62565c3",
+    "url": "/static/media/66logo.7650d1a9.png"
+  },
+  {
+    "revision": "5c3f9ab6315c0714f9e6b40c8d1f3512",
+    "url": "/static/media/MaiDang2020.5c3f9ab6.pdf"
+  },
+  {
+    "revision": "f3b613ae274749b33f667568529dd6b7",
+    "url": "/static/media/aboutPic.f3b613ae.png"
+  },
+  {
+    "revision": "aaaf10d9b224b1cc46575862e0c41bf3",
+    "url": "/static/media/community.aaaf10d9.png"
+  },
+  {
+    "revision": "6ef6b74a7b247356268c609eaceb6b42",
+    "url": "/static/media/count.6ef6b74a.png"
+  },
+  {
+    "revision": "2b893c6ba1dfd81f5e38484611e1d5b2",
+    "url": "/static/media/demoAccount.2b893c6b.png"
+  },
+  {
+    "revision": "1f1331bfed269e1efce8288f6aff825a",
+    "url": "/static/media/intern.1f1331bf.png"
+  },
+  {
+    "revision": "29c6891263eda97fcfb3dd5dd62bf635",
+    "url": "/static/media/team.29c68912.png"
+  },
+  {
+    "revision": "6309a345344cb49adf1844fd9790522c",
+    "url": "/static/media/zestIcon.6309a345.png"
+  }
+]);
